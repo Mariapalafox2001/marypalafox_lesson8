0298deebdb0de8b4be66e2826d0299dab1b5d7e5
Mariapalafox2001 <43785186+Mariapalafox2001@users.noreply.github.com>
2018-12-12 12:48:04 -0500

    Add files via upload

---
+++ lesson_8/Lesson8_problem1.py
@@ -0,0 +1,9 @@
+age = input (' what is your age? ')
+age = int(age)
+birthyear =  2018 - age
+answer = birthyear + 79
+print('...Thinking...')
+print('...Thinking...')
+print('...Thinking...')
+print('you will die in the year...')
+print(answer)
